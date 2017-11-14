50794f561e1770dfa8418c9e67a4f111126f7e84
jjmschofield <jjmschofield@googlemail.com>
2017-11-14 13:14:49 +0000

    Adding app.js to initialize graphql on the root path using the schema

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
-/**
- * Created by jack on 14/11/2017.
- */
+'use strict';
+
+const express = require('express');
+const body_parser = require('body-parser');
+
+const expressGraphQL = require('express-graphql');
+
+// let's import the schema file we just created
+const GraphQLSchema = require('./graphql/schema');
+
+
+const app = express();
+
+app.use( body_parser.json({ limit: '50mb' }) );
+
+app.use(
+    '/',
+    expressGraphQL( () => {
+        return {
+            graphiql: true,
+            schema: GraphQLSchema,
+        }
+    })
+);
+
+module.exports = app;
