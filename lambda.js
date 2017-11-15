76f4a966bab5d8611ceaa1271a00748ba2e676db
jjmschofield <jjmschofield@googlemail.com>
2017-11-15 11:54:32 +0000

    Adding lamda exports

--- lambda.js
+++ lambda.js
@@ -1,3 +1,7 @@
-/**
- * Created by jack on 14/11/2017.
- */
+'use strict'
+
+const awsServerlessExpress = require('aws-serverless-express');
+const app = require('./src/app');
+const server = awsServerlessExpress.createServer(app);
+
+exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context);
